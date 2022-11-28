0f7bca2fa6c9a73bb770e53a7af9602b33c14b01
matthew-lui <107003143+matthew-lui@users.noreply.github.com>
2022-11-28 14:15:24 -0500

    commit tree da67159c09a1f6660b2499cb098df33077e364a0

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,8 @@
 class Character < ActiveRecord::Base
-  
-end
+  belongs_to :actor
+  belongs_to :show
+  def say_that_thing_you_say
+    #run the name getting function, does a select on the database and gets character ID and grabs name and returns string
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+end 
